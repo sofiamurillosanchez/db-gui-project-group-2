edcaaa3f18c51db90372ed649578183d5c063735
leilaniGegg <lguzman@smu.edu>
2022-11-03 12:09:48 -0500

    Add files via upload

--- backend/middleware/model-middleware.js
+++ backend/middleware/model-middleware.js
@@ -1,10 +1,13 @@
 const User = require('../models/users');
+const Event = require('../models/event');
 const createModelsMiddleware = async (req, res, next) => {
    req.models = {
-      users: User
+      users: User,
+      event: Event
   }
   next();
 }
+
 module.exports = {
   createModelsMiddleware
 }
