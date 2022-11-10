670659e5a674cc0253b53eb5759ace577307b78a
leilaniGegg <lguzman@smu.edu>
2022-11-09 22:21:53 -0600

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const usersRoutes = require('./routes/user' );
-const sessionRoutes = require('./routes/session' );
-const eventRoutes = require('./routes/events' );
+//const sessionRoutes = require('./routes/session' );
+const eventRoutes = require('./routes/event' );
+const ticketRoutes = require('./routes/ticket' );
+const businessRoutes = require('./routes/business' );
+const reportRoutes = require('./routes/report' );
+const event_requestRoutes = require('./routes/event_request' );
 const { authenticateJWT , authenticateWithClaims  } = require('./middleware/auth' );
 const { createModelsMiddleware  } = require('./middleware/model-middleware' );
 const app = express();
@@ -15,9 +19,13 @@ app.get('/health', (request, response, next) => {
   response.json(responseBody);
   next();
 });
-app.use('/session', sessionRoutes);
+//app.use('/session', sessionRoutes);
 app.use('/user', authenticateJWT , usersRoutes);
-app.use('/events', eventRoutes);
+app.use('/event', eventRoutes);
+app.use('/ticket', ticketRoutes);
+app.use('/business', businessRoutes);
+app.use('/report', reportRoutes);
+app.use('/event_request', event_requestRoutes);
 app.listen(port, () => {
   console.log(`This app is listening on port  ${port}`);
 });
