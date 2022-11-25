33b5a1535e394a3698953f37a89ab264c63a555b
rafe-forward <rforward@smu.edu>
2022-11-25 17:40:54 -0500

    component
    fixed

---
+++ frontend/my-app/src/models/User.js
@@ -0,0 +1,9 @@
+export class User {
+    constructor(username, password, email, phone_number, role){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.role = role;
+    }
+}
