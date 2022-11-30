9c23c74fda7f168af243e9e5790c0232e76349f4
johnjoestar <60397553+johnjoestar@users.noreply.github.com>
2022-11-29 22:01:42 -0600

    landing page updated

--- frontend/landing_page.js
+++ frontend/landing_page.js
@@ -10,3 +10,22 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction:false,
+    }
+});
