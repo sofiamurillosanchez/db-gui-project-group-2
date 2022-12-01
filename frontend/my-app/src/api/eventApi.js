685aba36c8c7291ea989f13729dbdde99f5bd713
johnjoestar <60397553+johnjoestar@users.noreply.github.com>
2022-12-01 15:59:49 -0600

    update

--- frontend/my-app/src/api/eventApi.js
+++ frontend/my-app/src/api/eventApi.js
@@ -1,13 +1,6 @@
 import axios from "axios";
 import {url} from "../url/url";
 
-// import {url} from "../App"
-const ec2_url = 'ec2-52-205-137-65.compute-1.amazonaws.com'
-  // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
-const ec2 = true;
-  // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
-const url = ec2 ? ec2_url : 'localhost'
-
 /*User API */
 export const getAuthenticatedUser = () => new Promise((resolve, reject) => {
     axios.get(`http://${url}:8000/authenticateUser`)
